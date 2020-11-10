c98b2ab32295c54bbd034f96bee4b8526e9b850f
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-11-10 19:01:50 +0000

    Made our data persistent as in saving our URLs and redirected the shortURL to the actual website(longURL)

--- express_server.js
+++ express_server.js
@@ -1,22 +1,24 @@
 const express = require("express"); // importing all required modules
-const app = express();
 const bodyParser = require("body-parser");
+const app = express();
+
 app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs"); // setting ejs as the view engine
 
-const generateRandomString = function(length, char) {
+const generateRandomString = function(length) {
   let string = '';
+  const char = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
   for (let i = length; i > 0; i--) {
     string += char[Math.floor(Math.random() * char.length)];
-    return string;
   }
+  return string;
 }
-generateRandomString(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+generateRandomString(6)
 
 
-const urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -46,19 +48,30 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(req.params)
+ // console.log(req.params)
   // console.log(req);
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 //posting new url
 app.post("/urls", (req, res) => {
-  const urlShort = generateRandomString()
-  urlDatabase[urlShort] = {};
-  urlDatabase[urlShort].longURL = req.body.longURL;
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString(6);
+  console.log('Generate random sting', shortURL);
+  const longURL = req.body.longURL;
+ // console.log(shortURL);
+  console.log(longURL);
+  //console.log(urlDatabase);
+  urlDatabase[shortURL] = longURL;
+ console.log(urlDatabase);
+  res.redirect('/urls/:shortURL');
+  //console.log(req.body);  // Log the POST request body to the console
+ // res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
