73764834d77b2639475b345afa6bc7f6dd3fd300
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-11-11 05:33:36 +0000

    Added new features, like post - edit delete, login and logout

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express"); // importing all required modules
 const bodyParser = require("body-parser");
 const app = express();
+const cookieParser = require("cookie-parser")
 
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080; // default port 8080
 
@@ -24,6 +26,7 @@ let urlDatabase = {
 };
 
 // Routing functions :-
+
 //homepage
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -37,20 +40,21 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-//createing new url page
+//creating new url page
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies['username'] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies['username'], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
  // console.log(req.params)
   // console.log(req);
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { username: req.cookies['username'], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
@@ -61,18 +65,48 @@ app.get("/u/:shortURL", (req, res) => {
 
 //posting new url
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString(6);
-  console.log('Generate random sting', shortURL);
-  const longURL = req.body.longURL;
+  let shortURL = generateRandomString(6);
+  //console.log('Generate random sting', shortURL);
+  let longURL = req.body.longURL;
  // console.log(shortURL);
-  console.log(longURL);
+  //console.log(longURL);
   //console.log(urlDatabase);
   urlDatabase[shortURL] = longURL;
- console.log(urlDatabase);
-  res.redirect('/urls/:shortURL');
+ //console.log(urlDatabase);
+  res.redirect(`/urls/${shortURL}`);
   //console.log(req.body);  // Log the POST request body to the console
  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
+
+
+//deleting A URL
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls")
+});
+
+app.post('/urls/:shortURL', (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect(`/urls/${req.params.shortURL}`)
+});
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username)
+  res.redirect("/urls")
+  // console.log(username)
+  const templateVars = {
+    username: req.cookies["username"],
+    // ... any other vars
+  };
+  res.render("urls_index", templateVars);
+  })
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username'); //res.clearCookie('name'
+  // res.cookie('username', req.body.username);
+  res.redirect("/urls")
+  })
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
